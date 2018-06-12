c40d80c67ca664166456130a8a966b61ca48089c
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2018-06-11 21:40:41 -0500

    avance cargar varias imagenes

---
+++ common/data/src/main/java/org/thingsboard/server/common/data/Image.java
@@ -0,0 +1,54 @@
+package org.thingsboard.server.common.data;
+
+public class Image {
+    private String name;
+    private String latitude;
+    private String longitude;
+    private String fileSize;
+    private String modifiedDate;
+
+    public Image(){
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
--- dao/src/main/java/org/thingsboard/server/dao/mongo/MongoDbImage.java
+++ dao/src/main/java/org/thingsboard/server/dao/mongo/MongoDbImage.java
@@ -1,11 +1,13 @@
 package org.thingsboard.server.dao.mongo;
 
+import com.drew.metadata.Tag;
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
 import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.GridFSFindIterable;
 import com.mongodb.client.gridfs.model.GridFSDownloadByNameOptions;
 import com.mongodb.client.gridfs.model.GridFSFile;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
@@ -18,19 +20,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.HashMap;
+import java.util.*;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import com.drew.metadata.Metadata;
 
 import com.mongodb.client.model.Filters;
+import org.apache.tomcat.jni.Directory;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.aspectj.util.FileUtil;
 import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.thingsboard.server.common.data.Image;
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
 import static sun.security.krb5.Confounder.bytes;
 //import org.apache.commons.io;
 
@@ -149,5 +154,70 @@ public class MongoDbImage extends MongoConnection {
         } catch (IOException e) {
             throw new MongoDBException("It wasn´t posible to load the file");
         }
+    }
+
+
+
+
+
+    public Map<Image, String> downloadMapsFile(String parcelId, String date) throws Exception {
+        GridFSFindIterable gridFSFiles = getGridFSDatabase().find(Filters.and(Filters.eq("metadata.ParcelId", parcelId),Filters.eq("metadata.Date", date)));
+        Map<Image, String> data = new HashMap<>();
+        for (GridFSFile gridFSFile: gridFSFiles){
+            try {
+                Image image= new Image();
+                FileOutputStream streamToDownloadTo = new FileOutputStream( gridFSFile.getFilename());
+                getGridFSDatabase().downloadToStream(gridFSFile.getFilename(), streamToDownloadTo);
+                streamToDownloadTo.close();
+                System.out.println(streamToDownloadTo.toString());
+
+
+
+                File f = new File( gridFSFile.getFilename());
+                String resultBase64Encoded = "";
+                if (f.exists()) {
+                    resultBase64Encoded = Base64.getEncoder().encodeToString(org.apache.commons.io.FileUtils.readFileToByteArray(f));
+                    f.delete();
+                } else {
+                    System.out.println("no existe el archivo");
+                }
+
+                Metadata metadata = JpegMetadataReader.readMetadata(f);
+
+                image=setImageValues(metadata,image);
+
+                data.put(image,resultBase64Encoded);
+
+            } catch (IOException e) {
+                throw new MongoDBException("It wasn´t posible to load the file");
+            }
+
+        }
+        return data;
+    }
+
+
+    private Image setImageValues(Metadata metadata, Image img){
+        for (com.drew.metadata.Directory directory : metadata.getDirectories()) {
+            for (Tag tag : directory.getTags()) {
+                if(tag.getTagName().equals("GPS Latitude")){
+                    img.setLatitude(tag.getDescription());
+                    System.out.println("    "+tag.getDescription());
+                }else if(tag.getTagName().equals("GPS Longitude")){
+                    img.setLongitude(tag.getDescription());
+                    System.out.println("    "+tag.getDescription());
+                }else if(tag.getTagName().equals("File Modified Date")){
+                    img.setModifiedDate(tag.getDescription());
+                    System.out.println("    "+tag.getDescription());
+                }else if(tag.getTagName().equals("File Size")){
+                    img.setFileSize(tag.getDescription());
+                    System.out.println("    "+tag.getDescription());
+                }else if(tag.getTagName().equals("File Name")){
+                    img.setName(tag.getDescription());
+                    System.out.println("    "+tag.getDescription());
+                }
+            }
+        }
+        return img;
     }
 }
